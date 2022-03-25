362c30e90608952d29b761680d44922b7e1ea2a0
bran3t <alex.bran3t@gmail.com>
2022-03-25 18:46:57 +0300

    Verificati va rog tema pe acasa: getter si setter

---
+++ src/com/company/topic2/Main.java
@@ -0,0 +1,11 @@
+package com.company.topic2;
+
+public class Main {
+    public static void main(String[] args) {
+        Persona marinelPersona = new Persona();
+
+        marinelPersona.numePersona = "Marin";
+        marinelPersona.setVirsta(26);
+        System.out.println(marinelPersona.getVirsta());
+    }
+}
